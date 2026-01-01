3c15d09f507c580622d7100c06849f33fa76f32c
Hyrenal <h.chaloyan@gmail.com>
2025-12-31 22:37:18 -0800

    Added flask-cors

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 
 # Imports
-from flask import Flask
+from flask import Flask, send_from_directory
+from flask_cors import CORS
 from app.routes.workouts import workouts_bp
 from app.routes.exercises import exercises_bp
 import sqlite3
@@ -9,7 +10,10 @@ import sqlite3
 
 
 # Initialize Flask app
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../mobile/myApp/screens', static_url_path='/')
+
+# Allows requests from the app
+CORS(app)
 
 # Register Blueprints
 app.register_blueprint(workouts_bp)
@@ -19,8 +23,8 @@ app.register_blueprint(exercises_bp)
 
 # HOME PAGE
 @app.route('/')
-def index():
-    return "HOME PAGE HI"
+def home():
+    return send_from_directory(app.static_folder, 'HomeScreen.js')
 
 # EDIT LATER, EITHER POST OR GET
 @app.route('/settings')
